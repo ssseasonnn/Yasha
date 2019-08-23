4d5822e9aba09e1316eed0765e7e6c278759f1be
ssseasonnn <ssseasonnn@gmail.com>
2019-08-23 22:50:26 +0800

    add auto clean up mechanism.

--- app/src/main/java/zlc/season/yaksaproject/DemoItem.kt
+++ app/src/main/java/zlc/season/yaksaproject/DemoItem.kt
@@ -1,9 +1,27 @@
 package zlc.season.yaksaproject
 
+import android.util.Log
+import io.reactivex.Flowable
+import io.reactivex.disposables.Disposable
 import zlc.season.yasha.YashaItem
+import java.util.concurrent.TimeUnit
 
 class NormalItem(val i: Int) : YashaItem {
     override fun toString() = "Item $i"
+
+    private val disposable: Disposable
+
+    init {
+        //Test auto clean up!!!!
+        disposable = Flowable.interval(0, 1, TimeUnit.SECONDS)
+                .subscribe {
+                    Log.d("YASHA", "$it")
+                }
+    }
+
+    override fun cleanUp() {
+        disposable.dispose()
+    }
 }
 
 class HeaderItem(val i: Int) : YashaItem {
