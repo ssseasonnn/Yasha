940e552977586e7d8a4f84c3c1562409adfd9db9
ssseasonnn <ssseasonnn@gmail.com>
2021-02-07 16:02:43 +0800

    fix load next

--- yasha/src/main/java/zlc/season/yasha/YashaDataSource.kt
+++ yasha/src/main/java/zlc/season/yasha/YashaDataSource.kt
@@ -3,6 +3,11 @@ package zlc.season.yasha
 import zlc.season.sange.SangeDataSource
 
 open class YashaDataSource : SangeDataSource<YashaItem>() {
+
+    override fun shouldLoadNext(position: Int): Boolean {
+        return position == itemSize() - 1
+    }
+
     override fun onStateChanged(newState: Int) {
         setState(YashaStateItem(newState, ::retry))
     }
